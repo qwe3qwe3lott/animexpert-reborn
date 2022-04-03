32fcf66f5edc9d6093f6cdbd86b29ff6e726330d
BuildTools <unconfigured@null.spigotmc.org>
2022-04-03 17:24:38 +0300

    v. 0.0.0.0.1 "anime__zabanili" update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -2,19 +2,20 @@ import React from 'react';
 import {NavLink} from 'react-router-dom';
 
 import styles from './Header.module.scss';
+import logo from '../../assets/images/logo.webp';
 
-const setActive = ({isActive}: { isActive: boolean }): string => isActive ? styles.activeNavLink : '';
+const setActive = ({isActive}: { isActive: boolean }): string => isActive ? [styles.navLink, styles.activeNavLink].join(' ') : styles.navLink;
 
 class Header extends React.Component {
 	render() {
 		return (<>
 			<header className={styles.header}>
-				<div className={styles.logo}>logo</div>
+				<div className={styles.logo}><img alt="logo" src={logo} className={styles.logoImage}/></div>
 				<div className={styles.auth}>auth</div>
 				<nav className={styles.nav}>
-					<NavLink className={setActive} to="/">Home</NavLink>
-					<NavLink className={setActive} to="/review">Review</NavLink>
-					<NavLink className={setActive} to="/tier">Tier</NavLink>
+					<NavLink className={setActive} to="/"><span>Home</span><span className={styles.navSpan}/></NavLink>
+					<NavLink className={setActive} to="/review"><span>Review</span><span className={styles.navSpan}/></NavLink>
+					<NavLink className={setActive} to="/tier"><span>TierList</span><span className={styles.navSpan}/></NavLink>
 				</nav>
 			</header>
 		</>);
